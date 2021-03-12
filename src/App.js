75c62955fde1e07e567834228650856820bd6470
LCBradley3k <bradleymitchellis@gmail.com>
2021-03-11 19:20:16 -0800

    new playful background image

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import VideoPlayer from './VideoPlayer'
 
 function App() {
+  const urls = [
+    "https://youtu.be/gNf8Bsu0UYc"
+  ];
+
   return (
     <div className="App">
       <div className="App__Header">
         <h1>Gibberish.TV</h1>
         <h2>Unwind an exhausted mind</h2>
       </div>
-      <VideoPlayer url="https://www.youtube.com/watch?v=gNf8Bsu0UYc" />
+      <VideoPlayer urls={urls} />
       <footer> The Footer. </footer>
     </div>
   );
--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,29 +1,44 @@
 import { render } from "@testing-library/react";
-import React from "react";
+import React, {Component} from "react";
 import ReactPlayer from 'react-player/youtube'
 import "./VideoPlayer.css"
 
-function VideoPlayer(props) {
+class VideoPlayer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            currentUrl: ""
+        }
+        this.nextVideo = this.nextVideo.bind(this);
 
-    const urls = [
-        "https://youtu.be/gNf8Bsu0UYc"
-    ];
-    
-    return(
-        <div className="VideoPlayer">
-            <ReactPlayer
-                className="react-player"
-                url={urls}
-                controls={false}
-                playing={true}
-                config={{
-                    youtube: {
-                        autoplay: 1
-                    }
-                }}
-            />
-        </div>
-    )
+    }
+
+    nextVideo(currentVideo) {
+        this.setState({currentUrl: this.props.urls[0]});
+    }
+
+    render() {
+        const urls = this.props.urls;
+        //const _currUrl = this.nextVideo(null) // no video played yet.
+        
+        return(
+            <div className="VideoPlayer">
+                <ReactPlayer
+                    className="react-player"
+                    url={urls}
+                    controls={false}
+                    playing={true}
+                    config={{
+                        youtube: {
+                            autoplay: 1,
+                            modestbranding: 1
+                        }
+                    }}
+                />
+                {/*<button>next (random) gibberish &#x2192;</button>*/}
+            </div>
+        )
+    }
 }
 
 export default VideoPlayer;
